57500b01a86fc9d5f930eeaaed2547f908b7811a
thermvs <mariagewitter@gmail.com>
2023-12-04 20:18:20 +0300

    feat: add basic structure

---
+++ src/main/java/store/furniture/WoodcraftFurnitureStoreApplication.java
@@ -0,0 +1,13 @@
+package store.furniture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodcraftFurnitureStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoodcraftFurnitureStoreApplication.class, args);
+    }
+
+}
